ec91f81604374f29a104d36b5cf050f877593e25
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-04 14:57:39 -0700

    refactor editing function with firebase
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -57,24 +57,28 @@ class TicketControl extends React.Component {
     dispatch(action);
   }
 
-  // handleChangingSelectedTicket = (id) => {
-  //   const selectedTicket = this.props.masterTicketList[id];
-  //   this.setState({selectedTicket: selectedTicket});
-  // }
+  handleChangingSelectedTicket = (id) => {
+    this.props.firestore.get({collection: 'tickets', doc: id}).then((ticket) => {
+      const firestoreTicket = {
+        names: ticket.get("names"),
+        location: ticket.get("location"),
+        issue: ticket.get("issue"),
+        id: ticket.id
+      }
+      this.setState({selectedTicket: firestoreTicket });
+    });
+  }
 
   handleEditClick = () => {
     this.setState({editing: true});
   }
 
-  // handleEditingTicketInList = (ticketToEdit) => {
-  //   const { dispatch } = this.props;
-  //   const action = a.addTicket(ticketToEdit);
-  //   dispatch(action);
-  //   this.setState({
-  //     editing: false,
-  //     selectedTicket: null
-  //   });
-  // }
+  handleEditingTicketInList = () => {
+    this.setState({
+      editing: false,
+      selectedTicket: null
+    });
+  }
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
